ab48285828321ad6a931491b9f2d7746ea79aece
Mohamed Eladly <msrtv365@yahoo.com>
2023-10-03 17:04:27 +0300

    Solved the second advanced task

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    for i in range(len(str)):
+        if (i == n):
+            str = str[:i] + str[i + 1:]
+            return str
+    else:
+        return str
