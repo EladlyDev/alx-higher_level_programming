46db4e2bcd581abb70672d66b427ae8dfcab3629
Mohamed Eladly <msrtv365@yahoo.com>
2023-10-07 10:26:59 +0300

    Fixing efficiency

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,43 +1,19 @@
 #include "lists.h"
 int is_palindrome(listint_t **head)
 {
-	int *arr, len, i;
+	int arr[5000], len, i;
 	listint_t *tmp = *head;
 
-	len = linked_length(*head);
-	if (len == 0)
+	if (!*head)
 		return (1);
-	arr = malloc(sizeof(int) * len);
-	for (i = 0; tmp; i++)
+	for (len = 0; tmp; len++)
 	{
-		arr[i] = tmp->n;
+		arr[len] = tmp->n;
 		tmp = tmp->next;
 	}
 	for (i = 0, tmp = *head, len--; tmp; tmp = tmp->next, i++, len--)
-	{
 		if (tmp->n != arr[len])
-		{
-			free(arr);
 			return (-1);
-		}
-	}
 
-	free(arr);
 	return (1);
-}
-
-
-int linked_length(listint_t *head)
-{
-	listint_t *tmp = head;
-	int count = 0;
-
-	if (!head)
-		return (0);
-	while (tmp)
-	{
-		count++;
-		tmp = tmp->next;
-	}
-	return (count);
 }
