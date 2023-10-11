f91618379079a6650dc65e587ec8972b8aca8ded
Mohamed Eladly <msrtv365@yahoo.com>
2023-10-11 16:59:16 +0300

    Solved the third advanced task

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if a_dictionary:
+        for key, val in (a_dictionary.copy()).items():
+            if val == value:
+                del a_dictionary[key]
+    return a_dictionary
