87a4d012de298ba5979f058ac2e2db0b546ee5a6
Mohamed Eladly <msrtv365@yahoo.com>
2023-10-11 18:00:18 +0300

    removed the for loop - required

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return(list(map(lambda row: [x*x for x in row], [row for row in matrix])))
+    return(list(map(lambda row: list(map(lambda x: x*x, row)), matrix)))
