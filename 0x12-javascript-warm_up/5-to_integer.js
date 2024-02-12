246666ef20752d1e6b79bdeb47988ad3f0b7ae86
Mohamed Eladly <msrtv365@yahoo.com>
2024-02-12 16:23:12 +0200

    mend

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,6 @@
 const argv = process.argv;
 const no = parseInt(argv[2]);
 
-if (no)
-{
-    console.log('My number: ' + no);
+if (no) {
+  console.log('My number: ' + no);
 } else { console.log('Not a number'); }
