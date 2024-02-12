58da60f0a260965968143232f174dc939d750d63
Mohamed Eladly <msrtv365@yahoo.com>
2024-02-12 16:08:52 +0200

    Trying to fix the last two tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,8 @@
 const argv = require('node:process').argv;
 const arglen = argv.length;
 
-if (arglen < 3) {
-  console.log('No argument');
+if (arglen > 3) {
+  console.log('Arguments found');
 } else if (arglen === 3) {
   console.log('Argument found');
-} else { console.log('Arguments found'); }
+} else { console.log('No argument'); }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,6 @@
 
 const argv = require('node:process').argv;
 
-if (!argv[2]) {
-  console.log('No argument');
-} else if (argv[2]) {
+if (argv[2]) {
   console.log(argv[2]);
-}
+} else { console.log('No argument'); }
