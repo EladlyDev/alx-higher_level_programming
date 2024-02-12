a6684a9abc27cf33ef5f871fedf627cb20bfd1f6
Mohamed Eladly <msrtv365@yahoo.com>
2024-02-12 16:56:25 +0200

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+let size = parseInt(argv[2]);
+const out = (size >= 0) ? 'X'.repeat(size) : "";
+
+if (size) {
+  while (size > 0) {
+    console.log(out);
+    size--;
+  }
+} else { console.log('Missing number of occurrences'); }
