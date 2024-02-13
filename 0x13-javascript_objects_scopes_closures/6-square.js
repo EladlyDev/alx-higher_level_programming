6e54782b8172f9a7889a89b2b6bf746a4da32a12
Mohamed Eladly <msrtv365@yahoo.com>
2024-02-13 11:41:17 +0200

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      let h = this.height;
+      const w = this.width;
+      const out = c.repeat(w);
+
+      while (h) { console.log(out); h--; }
+    } else { this.print(); }
+  }
+}
+
+module.exports = Square;
