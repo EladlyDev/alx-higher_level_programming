02745600ee93bb74d18cdceefb0d1a63887160c8
Mohamed Eladly <msrtv365@yahoo.com>
2024-02-13 15:45:41 +0200

    13. Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const fs = require('node:fs');
+const args = require('node:process').argv;
+
+if (args.length === 5) {
+  const f1Data = fs.readFileSync(args[2], 'utf-8');
+  const f2Data = fs.readFileSync(args[3], 'utf-8');
+  const out = f1Data + f2Data;
+  fs.writeFileSync(args[4], out);
+}
