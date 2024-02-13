5a454941a3161d54bb7a4572addcfc34479695d2
Mohamed Eladly <msrtv365@yahoo.com>
2024-02-13 13:29:08 +0200

    12. Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+
+const out = {};
+
+Object.keys(dict).forEach((key) => {
+  const value = dict[key];
+  if (!out[value]) {
+    out[value] = [key];
+  } else {
+    out[value].push(key);
+  }
+});
+
+console.log(out);
