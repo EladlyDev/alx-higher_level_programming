462327430951656c06066c5e2f9dc0528df01a3e
Mohamed Eladly <msrtv365@yahoo.com>
2024-02-13 12:40:06 +0200

    9. Log me

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+class Log {
+  constructor (item) {
+    if (!Log.occ) Log.occ = 0;
+    console.log(`${Log.occ}: ${item}`);
+    Log.occ++;
+  }
+}
+
+exports.logMe = function (item) {
+  return new Log(item);
+};
