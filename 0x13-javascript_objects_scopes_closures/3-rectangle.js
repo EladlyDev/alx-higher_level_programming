d411165bfbeb021bdd978afe4e4797059ab666c5
Mohamed Eladly <msrtv365@yahoo.com>
2024-02-13 10:55:18 +0200

    3. Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    if (w > 0 & h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let h = this.height ? this.height : 0;
+    const w = this.width ? this.width : 0;
+    const out = 'X'.repeat(w);
+
+    while (h) { console.log(out); h--; }
+  }
+}
+
+module.exports = Rectangle;
