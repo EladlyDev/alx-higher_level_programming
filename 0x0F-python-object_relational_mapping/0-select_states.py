7f9c8e1de4e0db499a398a88b8950da5680cd0e4
Mohamed Eladly <msrtv365@yahoo.com>
2024-02-18 01:00:39 +0200

    mend

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """ 0-select_states.py """
-import mysql.connector as MySQLdb # cuz I had problem installing mysqldb
+# import mysql.connector as MySQLdb # cuz I had problem installing mysqldb
+import MySQLdb
 from sys import argv
 
-HOST = 'localhost'
-PORT = 3306
+if __name__ == "__main__":
+    HOST = 'localhost'
+    PORT = 3306
 
-if len(argv) >= 4:
+    if len(argv) >= 4:
         USER = argv[1]
         PASS = argv[2]
         DB = argv[3]
 
-        conn = MySQLdb.connect(host=HOST, port=PORT, user=USER, passwd=PASS,
-                               db=DB)
+        conn = MySQLdb.connect(host=HOST, port=PORT, user=USER,
+                               passwd=PASS, db=DB)
         cur = conn.cursor()
         cur.execute("SELECT * FROM states")
         states = cur.fetchall()
         [print(state) for state in states]
-else:
+    else:
         print("USAGE: {user} {pass} {db}")
