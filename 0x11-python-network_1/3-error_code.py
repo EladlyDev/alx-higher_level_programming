f160fb10a19c712f2087f2f423fa3135896afd5a
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-01 13:40:02 +0200

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" sends a request to the URL and displays the body of the response """
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+from sys import argv
+
+if __name__ == "__main__":
+    req = Request(argv[1])
+    try:
+        with urlopen(req) as res:
+            print(res.read().decode('utf-8'))
+    except HTTPError as e:
+        print(f"Error code: {e.code}")
