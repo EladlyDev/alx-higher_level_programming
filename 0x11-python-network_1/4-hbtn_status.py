59e83bbdf580bb2dc55842e185b6578648f0c403
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-01 19:25:06 +0200

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" fetches https://alx-intranet.hbtn.io/status """
+import requests
+
+
+if __name__ == "__main__":
+    content = requests.get("https://alx-intranet.hbtn.io/status").text
+    out = f"""Body response:\n\t- type: {type(content)}\n\t-\
+ content: {content}"""
+    print(out)
