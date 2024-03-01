8c8a26e7573c48d18185072c4c7e069d07adbbe6
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-01 13:20:56 +0200

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" sends a POST request to the passed URL with the email as a parameter """
+import urllib.parse
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': argv[2]}).encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as r:
+        print(r.read().decode("utf-8"))
