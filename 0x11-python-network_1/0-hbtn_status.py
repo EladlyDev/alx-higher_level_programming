7a78f18178e27fd86ef2fb9d5e63df4199f94458
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-01 12:24:05 +0200

    1. Response header value #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+""" that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
 
-request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-
-with urllib.request.urlopen(request) as r:
-    data = r.read()
-    out = f"""Body response:
-    - type: {type(data)}
-    - content: {data}
-    - utf8 content: {data.decode("utf-8")}"""
-    print(out)
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as r:
+        data = r.read()
+        out = f"""Body response:\n\t- type: {type(data)}\n\t- content: {data}\
+\n\t- utf8 content: {data.decode("utf-8")}"""
+        print(out)
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" sends a request to the URL and displays the value of the X-Request-Id
+variable found in the header of the response."""
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    request = urllib.request.Request(argv[1])
+
+    with urllib.request.urlopen(request) as r:
+        data = r.info().__dict__['_headers']
+        for d in data:
+            if d[0] == 'X-Request-Id':
+                print(d[1])
+                break
