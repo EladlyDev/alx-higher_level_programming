318bba81c4c17fa7dc6c861b677b2bf5db287f42
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-02 09:42:20 +0200

    7. Error code #1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,11 +10,14 @@ if __name__ == "__main__":
         url = argv[1]
     r = requests.post("http://0.0.0.0:5000/search_user",
                       data={'q': url})
-    if r.headers['Content-Type'] == "application/json":
-        dict = r.json()
-    else:
+
+    type = r.headers.get('Content-Type')
+    if not type or not type.startswith("application/json"):
         print("Not a valid JSON")
-        exit
+        exit(1)
+
+    dict = r.json()
+
     if dict:
         print(f"[{dict['id']}] {dict['name']}")
     else:
