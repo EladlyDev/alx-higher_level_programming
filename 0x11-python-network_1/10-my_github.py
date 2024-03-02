706039cfbe7b7012ac96adbc54ae28dce6c5c229
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-02 10:10:13 +0200

    9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" displays yoeur id using github api """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    header = {"Authorization": f"Bearer {argv[2]}"}
+    r = requests.get("https://api.github.com/user", headers=header)
+
+    dict = r.json()
+    if 'id' in dict:
+        print(dict['id'])
+    else:
+        print("None")
