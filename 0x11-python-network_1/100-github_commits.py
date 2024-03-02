e0e20d8db1e8895cdda0eaf2bc0f5a844284aba7
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-02 10:31:28 +0200

    10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" gets recent commits of the required repo """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    r = requests.get(url)
+
+    type = r.headers.get('Content-Type')
+    if not type or not type.startswith("application/json"):
+        exit(1)
+
+    i = 0
+    data = r.json()
+    for i in range(len(data)):
+        if i == 10:
+            break
+        print(f"{data[i]['sha']}: {data[i]['commit']['author']['name']}")
