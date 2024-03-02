2b02385a2922ebd74ee4dae86ac802c7daff0713
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-02 09:31:48 +0200

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" sends a POST request to the passed URL with the letter as a parameter """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = ""
+    if len(argv) >= 2:
+        url = argv[1]
+    r = requests.post("http://0.0.0.0:5000/search_user",
+                      data={'q': url})
+    if r.headers['Content-Type'] == "application/json":
+        dict = r.json()
+    else:
+        print("Not a valid JSON")
+        exit
+    if dict:
+        print(f"[{dict['id']}] {dict['name']}")
+    else:
+        print("No result")
