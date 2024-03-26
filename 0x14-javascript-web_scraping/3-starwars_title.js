43f4ad1ee47ffbe1e476919c1b04943b203764e6
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-26 20:36:11 +0200

    3. Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const request = require('request');
+const argv = require('process').argv;
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + argv[2];
+
+request(url, (err, res, body) => {
+  if (!err && body) console.log(JSON.parse(body).title);
+});
