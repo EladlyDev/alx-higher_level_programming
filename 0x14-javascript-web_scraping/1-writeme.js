03132a7c17b99bba0e69d47adc880170c3968e75
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-26 17:04:10 +0200

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const argv = require('process').argv;
+const fs = require('fs');
+
+const fname = argv[2];
+const data = argv[3];
+
+fs.writeFile(fname, data, err => {
+  if (err) {
+    console.log(err);
+  }
+});
