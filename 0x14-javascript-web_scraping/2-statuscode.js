1dd25aca47ec0413644736a688d103ff1e3e2e92
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-26 17:23:52 +0200

    2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const request = require('request');
+const argv = require('process').argv;
+
+const url = argv[2];
+
+request.get(url).on('response', res => {
+  console.log('code:', res.statusCode);
+});
