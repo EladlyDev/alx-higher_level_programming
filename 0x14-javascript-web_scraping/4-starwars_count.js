ebb2225ab4b39781049e88d2ff1be55bd20ba212
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-27 01:14:08 +0200

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+const argv = require('process').argv;
+
+const url = argv[2];
+
+request(url, (err, res, body) => {
+  if (!err) {
+    let out = 0;
+    JSON.parse(body).results.forEach(film => {
+      film.characters.forEach(char => {
+        if (char.includes('18')) { out++; }
+      });
+    });
+    console.log(out);
+  }
+});
