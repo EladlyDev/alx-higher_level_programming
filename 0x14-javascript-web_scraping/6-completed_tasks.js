60a67856985abb4ac81369a52f4c3f2aa32ba444
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-27 16:51:08 +0200

    6. How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const argv = require('process').argv;
+const request = require('request');
+
+const url = argv[2];
+
+request(url, (err, res, body) => {
+  if (!err) {
+    const tasks = JSON.parse(body);
+    const completed = {};
+
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].completed) {
+        if (tasks[i].userId in completed) { completed[tasks[i].userId]++; } else { completed[tasks[i].userId] = 1; }
+      }
+    }
+    console.log(completed);
+  }
+});
