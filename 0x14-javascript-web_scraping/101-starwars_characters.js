f90a5601344d1f7f77bee6533af5edc1286930c9
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-27 17:17:26 +0200

    8. Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const argv = require('process').argv;
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + argv[2];
+
+request(url, async (err, res, body) => {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const body = await new Promise((resolve, reject) => {
+        request(character, (err, res, body) => {
+          if (err) reject(err);
+          else resolve(body);
+        });
+      });
+      console.log(JSON.parse(body).name);
+    }
+  }
+});
