df9416886c23a5067d8cda43fab31f1b2698910a
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-27 15:18:00 +0200

    5. Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const argv = require('process').argv;
+const request = require('request');
+const fs = require('fs');
+
+const url = argv[2];
+const fName = argv[3];
+
+request.get(url).pipe(fs.createWriteStream(fName));
