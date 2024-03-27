7e5fcb020a065c32b6a0b0b66ad7c8a6aa39bf5b
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-27 17:07:23 +0200

    7. Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+const argv = require('process').argv;
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + argv[2];
+
+request(url, (err, res, body) => {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    for (const i in characters) {
+      request(characters[i], (err, res, body) => {
+        if (!err) console.log(JSON.parse(body).name);
+      });
+    }
+  }
+});
