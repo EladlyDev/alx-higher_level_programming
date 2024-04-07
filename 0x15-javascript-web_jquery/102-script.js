d9ba6c66cc3cc371dbec20a4711f11f62298de5b
Mohamed Eladly <msrtv365@yahoo.com>
2024-04-07 21:18:58 +0200

    12. Say hello to everybody!

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+$(document).ready(() => {
+  $('INPUT#btn_translate').on('click', () => {
+    const lang = $('INPUT#language_code').val()
+    $.get('https://hellosalut.stefanbohacek.dev/?lang=' + lang, data => {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
