66e196d2fc79cc50492ab556ce1256db98823190
Mohamed Eladly <msrtv365@yahoo.com>
2024-04-07 16:03:03 +0200

    8. Star Wars movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json',
+  data => {
+    for (const i in data.results) {
+      $('UL#list_movies').append('<li>' + data.results[i].title + '</li>');
+    }
+  });
