a08e01872e7a753bef84e7b1d6996581c9176bc3
Mohamed Eladly <msrtv365@yahoo.com>
2024-04-07 21:31:40 +0200

    13. And press ENTER

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+$(document).ready(() => {
+  const fetch = () => {
+    const lang = $('INPUT#language_code').val()
+    $.get('https://hellosalut.stefanbohacek.dev/?lang=' + lang, data => {
+      $('DIV#hello').text(data.hello);
+    });
+  }
+
+  $('INPUT#btn_translate').on('click', fetch);
+  $('INPUT#language_code').keypress((e) => {
+    if (e.which == 13) { fetch();}
+  });
+});
