ee6e4da8bdf2746cd40e34cd037aa91183f18ba7
Mohamed Eladly <msrtv365@yahoo.com>
2024-04-07 21:08:43 +0200

    11. List, add, remove

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+$(document).ready(() => {
+  const list = $('UL.my_list')
+  $('DIV#add_item').on('click', () => {
+    list.append('<li>Item</li>');
+  });
+  $('DIV#remove_item').on('click', () => {
+    $('UL.my_list :last-child').remove()
+  });
+  $('DIV#clear_list').on('click', () => {
+    list.empty()
+  });
+});
